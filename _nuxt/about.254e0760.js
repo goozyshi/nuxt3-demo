1241bdb7c31bba01b602d95ce511b4b3118207f9
goozyshi <39510459+goozyshi@users.noreply.github.com>
2023-06-25 01:42:06 +0800

    Updates

--- _nuxt/about.254e0760.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as s,o as c,F as _,p as n,d,a as o}from"./entry.d2479edf.js";const p={},t=e=>(n("data-v-a6295e24"),e=e(),d(),e),r=t(()=>o("h2",null,"About",-1)),u=t(()=>o("p",null," Lorem ipsum dolor sit amet, consectetur ",-1));function l(e,i){return c(),s(_,null,[r,u],64)}const f=a(p,[["render",l],["__scopeId","data-v-a6295e24"]]);export{f as default};
